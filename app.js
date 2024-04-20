d3979c7eeb0b413507f75674d38e44d5f0d9e2b4
micha <michael.andrew.glass@gmail.com>
2024-04-20 17:02:11 +0100

    log in and reg functionality added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 // Create Express application
 const app = express();
@@ -24,7 +26,74 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// Define routes
+// Set up session middleware
+app.use(session({
+    secret: 'session_key',
+    resave: false,
+    saveUninitialized: false
+  }));
+  
+  // Middleware to set req.user based on session data
+  app.use((req, res, next) => {
+    req.user = req.session.userId ? { id: req.session.userId } : null;
+    next();
+  });
+
+// post routes for handling data 
+app.post('/register', async (req, res) => {
+    try {
+      const hashedPassword = await bcrypt.hash(req.body.passwordReg, 10);
+      const userData = {
+        email: req.body.emailReg,
+        username: req.body.usernameReg,
+        password: hashedPassword,
+        registration_date: new Date()
+      };
+  
+      connection.query('INSERT INTO member SET ?', userData, (error, results) => {
+        if (error) {
+          console.error('Error inserting user:', error);
+          return res.redirect('/register');
+        }
+        console.log('User registered successfully');
+        res.redirect('/login');
+      });
+    } catch (error) {
+      console.error('Error registering user:', error);
+      res.redirect('/register');
+    }
+  });
+  
+app.post('/login', async (req, res) => {
+    const { usernameOrEmail, password } = req.body;
+    try {
+      const query = 'SELECT * FROM member WHERE email = ? OR username = ?';
+      connection.query(query, [usernameOrEmail, usernameOrEmail], async (error, results) => {
+        if (error) {
+          console.error('Error querying database:', error);
+          return res.status(500).send('Internal Server Error');
+        }
+        if (results.length === 0) {
+          return res.status(401).send('Invalid username or email');
+        }
+        const user = results[0];
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+          return res.status(401).send('Invalid password');
+        }
+        req.session.userId = user.member_id;
+        req.user = { id: user.member_id };
+        console.log("User signed in")
+        console.log(req.user)
+        res.render("home.ejs");
+      });
+    } catch (error) {
+      console.error('Error logging in:', error);
+      res.status(500).send('Internal Server Error');
+    }
+  });
+
+// Define get routes to render pages 
 app.get('/', (req, res) => {
     res.render('home'); 
 });
@@ -57,9 +126,6 @@ app.get('/all_collections', (req, res) => {
     res.render('all_collections'); 
 });
 
-// Define routes for members post log in
-
-
 // Handle contact form submission
 app.post('/contact', (req, res) => {
     const { name, email, message } = req.body;
@@ -76,12 +142,6 @@ app.post('/contact', (req, res) => {
     }, 1000); 
     
 });
-
-// database info 
-    //host: 'localhost',
-    //user: 'root',
-    //database: 'pok_project',
-    //password: 'xme%559MG'
 
 app.get('/register', (req, res)=>{
     res.render('register')
